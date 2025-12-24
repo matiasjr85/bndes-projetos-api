9fd4cd17ac4c12d429f84f4e310666555225cf09
Edmilson Matias <matiasjr@live.com>
2025-12-24 18:32:44 -0300

    Primeiro commit do projeto BNDES

---
+++ src/main/java/br/com/edmilson/bndes/projetos/api/BndesProjetosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.edmilson.bndes.projetos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BndesProjetosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BndesProjetosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edmilson/bndes/projetos/api/BndesProjetosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edmilson.bndes.projetos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BndesProjetosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
